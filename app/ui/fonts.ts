7b8d6fe8527448cccdd637d9fc77a15cf69673b8
joeday <thejode@gmail.com>
2023-12-19 12:26:47 -0800

    fix lusitana error

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,13 @@
-import { Inter, Cousine } from 'next/font/google';
+import { Inter, Cousine, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 export const cousine = Cousine({
   weight: ['400', '700'],
   style: ['normal', 'italic'],
   subsets: ['cyrillic'],
+});
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  style: ['normal', 'normal'],
+  subsets: ['latin'],
 });
