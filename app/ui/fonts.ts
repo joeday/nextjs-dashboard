f342093daa3b7fc6c7d6ae6c1540d203d4ef6bd0
joeday <thejode@gmail.com>
2023-12-19 12:17:20 -0800

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Cousine } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const cousine = Cousine({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['cyrillic'],
+});
